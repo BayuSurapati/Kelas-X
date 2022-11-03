4f98165a03029cf089965462ee2fe38f96da2050
BayuSurapati <93065357+BayuSurapati@users.noreply.github.com>
2022-11-03 08:26:04 +0700

    Add files via upload

---
+++ KELAS XI/KOTLIN/LATIHAN/LATIHAN1/ElseIf.kt
@@ -0,0 +1,11 @@
+fun main() {
+     val nilai = 90;
+
+    if(nilai > 85){
+        println("Bagus")
+    }else if(nilai >= 70){
+        println("Cukup")
+    }else{
+        println("Kurang Baik")
+    }
+}
